592a700eb4b8fb0d4c49e1337df33afc43b76888
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2023-10-08 16:18:44 +0400

    add sidebar items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Sidebar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      <li><a href="#">Главный экран</a></li>
+      <li><a href="#">Создать тренировку</a></li>
+      <li><a href="#">Последняя тренировка</a></li>
+      <li><a href="#">Следующая тренировка</a></li>
+      <li><a href="#">Календарь</a></li>
+      <li><a href="#">Список</a>
+        <ol>
+          <li>Тренировка 1</li>
+          <li>Тренировка 2</li>
+          <li>Тренировка 3</li>
+          <li>Тренировка 4</li>
+        </ol>
+      </li>
+      <li><a href="#">Мои шаблоны</a></li>
+    </ul>
   );
+}
+
+function App() {
+  return <Sidebar />;
 }
 
 export default App;
