a018b141e660ca08f48b1b96971154dc1a0b6c4c
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2023-10-30 00:04:44 +0400

    Work with form and State. Add form CreateTrain

---
+++ src/components/TrainingForm/TrainingForm.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function TrainingForm({ onSubmit }) {
+    const [form, setForm] = useState({
+        title: { value: '', type: 'text', label: 'Title' },
+        time: { value: '', type: 'time', label: 'Start Time' },
+        exercise: { value: '', type: 'text', label: 'Exercise Name' },
+        distance: { value: '', type: 'number', label: 'Distance (meters)' },
+        rest: { value: '', type: 'time', label: 'Rest (seconds)' },
+        numberApproaches: { value: '', type: 'number', label: 'Number Approaches' },
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setForm((prevForm) => ({
+            ...prevForm,
+            [name]: { ...prevForm[name], value },
+        }));
+    };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(form);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {Object.entries(form).map(([name, field]) => (
+            <div key={name}>
+                <label className="form-label">
+                    {field.label}:
+                </label>
+                <input
+                    type={field.type}
+                    name={name}
+                    value={field.value}
+                    onChange={handleChange}
+                    className="form-control"
+                />
+            </div>
+        ))}
+      <Button variant="primary" type="submit" className='mt-3'>Add Train</Button>
+    </form>
+  );
+}
--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,31 +1,42 @@
 import './calendar.css'
 import React, { useState } from 'react';
 
+import Modal from 'react-bootstrap/Modal';
+import TrainingForm from '../TrainingForm/TrainingForm'; // Импортируйте компонент TrainingForm
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './calendar.css';
 
 function Calendar() {
     const [week, setWeek] = useState({
-        "Monday": [{name: 'Train', time: '19:00 - 21:00'}],
+        "Monday": [],
         "Tuesday": [],
-        "Wednesday": [{name: 'Train', time: '19:00 - 21:00'}],
+        "Wednesday": [],
         "Thursday": [],
-        "Friday": [{name: 'Train', time: '19:00 - 21:00'}],
+        "Friday": [],
         "Saturday": [],
         "Sunday": [],
     });
 
-    function addTrain(day) {
-        const updatedWeek = { ...week };
-        
-        if (updatedWeek[day].length > 5) {
-            alert('Max 5 train in day');
-            return;
-        }
+    const [showModal, setShowModal] = useState(false);
+    const [selectedDay, setSelectedDay] = useState(null);
 
-        updatedWeek[day].push({name: 'New train'});
-
-        setWeek(updatedWeek);
-        console.log("add");
+    function handleShowModal(day) {
+        setSelectedDay(day);
+        setShowModal(true);
     }
+
+    function handleCloseModal() {
+        setShowModal(false);
+    }
+
+    const addTraining = (day, training) => {
+        setWeek((prevWeek) => {
+          const updatedDay = [...prevWeek[day], training];
+          return { ...prevWeek, [day]: updatedDay };
+        });
+        handleCloseModal();
+      };
 
     return (
         <div className="row">
@@ -40,20 +51,29 @@ function Calendar() {
                                     <div className='week-day__event-name'><i>Rest</i></div>
                                 ) : (
                                     events.map(event => (
-                                        <div key={event.name + event.time} className='week-day__event btn btn-outline-primary'>
-                                            <div className='week-day__event-name'>{event.name}</div>
-                                            <div className='week-day__event-time'>{event.time}</div>
+                                        <div key={event.name + event.time} className='btn btn-outline-primary week-day__event'>
+                                            <div className='week-day__event-name'>{event.title.value}</div>
+                                            <div className='week-day__event-time'>{event.time.value}</div>
                                         </div>
                                     ))
                                 )}
                             </div>
-                            <div className="week-day__add btn btn-light" onClick={() => addTrain(day)}>
+                            <div className="week-day__add btn btn-light" onClick={() => handleShowModal(day)}>
                                 <i className='fa-solid fa-plus'></i> Add Train
                             </div>
                         </div>
                     ))}
                 </div>
             </div>
+
+            <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                <Modal.Title>Add Train</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                <TrainingForm onSubmit={(training) => addTraining(selectedDay, training)} />
+                </Modal.Body>
+            </Modal>
         </div>
     );
 }
