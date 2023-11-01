5f25b5809c07559653da8b14be5af64814383804
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2023-11-02 00:36:42 +0400

    Add Date library + create calendar

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -7,7 +7,19 @@ import TrainingForm from '../TrainingForm/TrainingForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './calendar.css';
 
+import { format, startOfMonth, endOfMonth, eachDayOfInterval, addMonths, subMonths, getDay } from 'date-fns';
+
 function Calendar() {
+    const [currentMonth, setCurrentMonth] = useState(new Date());
+    const [events, setEvents] = useState({});
+    const [showModal, setShowModal] = useState(false);
+    const [editingTraining, setEditingTraining] = useState(null);
+
+    const startDay = startOfMonth(currentMonth);
+    const endDay = endOfMonth(currentMonth);
+    const daysInMonth = eachDayOfInterval({ start: startDay, end: endDay });
+    const firstDayOfWeek = getDay(startDay);
+
     const [week, setWeek] = useState({
         "Monday": [],
         "Tuesday": [],
@@ -18,7 +30,7 @@ function Calendar() {
         "Sunday": [],
     });
 
-    const [showModal, setShowModal] = useState(false);
+
     const [selectedDay, setSelectedDay] = useState(null);
 
     function handleShowModal(day) {
@@ -38,8 +50,6 @@ function Calendar() {
         handleCloseModal();
     };
     
-    const [editingTraining, setEditingTraining] = useState(null);
-
     const handleEditTraining = (day, training) => {
         setSelectedDay(day);
         setEditingTraining(training);
@@ -65,43 +75,57 @@ function Calendar() {
 
     return (
         <div className="row">
-            <div className="col-12">
-                <h1 className="title mb-3">Calendar</h1>
-                <div className='week mb-5'>
-                    {Object.entries(week).map(([day, events]) => (
-                        <div key={day} className="week-day">
-                            <div className='week-day__title'>{day}</div>
-                            <div className='week-day__events'>
-                                {events.length === 0 ? (
-                                    <div className='week-day__event-name'><i>Rest</i></div>
-                                ) : (
-                                    events.map(event => (
-                                        <div key={event.name + event.time} className='btn btn-outline-primary week-day__event'
-                                            onClick={() => handleEditTraining(day, event)}>
-                                            <div className='week-day__event-name'>{event.title.value}</div>
-                                            <div className='week-day__event-time'>{event.time.value}</div>
-                                        </div>
-                                    ))
-                                )}
+          <div className="col-12">
+            <h1 className="title mb-3">Calendar</h1>
+            
+            <h2 className='calendar-title'>
+                {format(currentMonth, 'MMMM yyyy')}
+
+                <div className="calendar-actions">
+                    <button className="btn btn-secondary calendar-actions__btn" onClick={() => setCurrentMonth(subMonths(currentMonth, 1))}><i class="fa-solid fa-backward"></i></button>
+                    <button className="btn btn-secondary calendar-actions__btn" onClick={() => setCurrentMonth(new Date())}>{format(currentMonth, 'MMMM yyyy')}</button>
+                    <button className="btn btn-secondary calendar-actions__btn" onClick={() => setCurrentMonth(addMonths(currentMonth, 1))}><i class="fa-solid fa-forward"></i></button>
+                </div>            
+            </h2>
+            <div className='calendar'>
+                {Array.from({ length: firstDayOfWeek }, (_, index) => (
+                    <div key={`empty-${index}`} className='week-day empty'></div>
+                ))}
+                {daysInMonth.map(day => (
+                <div key={day} className='week-day'>
+                    <div className='week-day__title'>{format(day, 'EEEE')}</div>
+                    <div className='week-day__title'>{format(day, 'dd')}</div>
+                    <div className='week-day__events'>
+                        {events[format(day, 'yyyy-MM-dd')] && events[format(day, 'yyyy-MM-dd')].length > 0 ? (
+                        events[format(day, 'yyyy-MM-dd')].map(event => (
+                            <div key={event.name + event.time} className='btn btn-outline-primary week-day__event'
+                                onClick={() => handleEditTraining(day, event)}>
+                            <div className='week-day__event-name'>{event.title.value}</div>
+                            <div className='week-day__event-time'>{event.time.value}</div>
                             </div>
-                            <div className="week-day__add btn btn-light" onClick={() => handleShowModal(day)}>
-                                <i className='fa-solid fa-plus'></i> Add Train
-                            </div>
-                        </div>
-                    ))}
+                        ))
+                        ) : (
+                        <div className='week-day__event-name'><i>Rest</i></div>
+                        )}
+                    </div>
+                    <div className="week-day__add btn btn-light" onClick={() => handleShowModal(day)}>
+                        <i className='fa-solid fa-plus'></i> Add Train
+                    </div>
                 </div>
+                ))}
             </div>
-
-            <Modal show={showModal} onHide={handleCloseModal}>
-                <Modal.Header closeButton>
-                <Modal.Title>{editingTraining ? 'Edit' : 'Add'} Train</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                <TrainingForm onSubmit={handleSubmit} editingTraining={editingTraining} />
-                </Modal.Body>
-            </Modal>
+          </div>
+    
+          <Modal show={showModal} onHide={handleCloseModal}>
+            <Modal.Header closeButton>
+              <Modal.Title>{editingTraining ? 'Edit' : 'Add'} Train</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <TrainingForm onSubmit={handleSubmit} editingTraining={editingTraining} />
+            </Modal.Body>
+          </Modal>
         </div>
-    );
-}
-
+      );
+    }
+    
 export default Calendar;
