bd52f6db5c03e5c7517f2cd89f4ea510437c0237
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2023-11-11 00:44:14 +0400

    Fix list og exercises

--- src/components/TrainingsList/TrainingsList.js
+++ src/components/TrainingsList/TrainingsList.js
@@ -12,6 +12,32 @@ function TrainingsList() {
         trainings: trainings[date]
     }));
 
+    function exercisesTable(exercises) {
+        if (!exercises || exercises.length === 0) return 'No exercises';
+        return (
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col" className='col-3'>Exercise Name</th>
+                        <th scope="col" className='col-3'>Distance (meters)</th>
+                        <th scope="col" className='col-3'>Rest (min)</th>
+                        <th scope="col" className='col-3'>Number Approaches</th>                      
+                    </tr>
+                </thead>
+                <tbody>
+                     {exercises.map((ex, index) => (                                                
+                        <tr key={ex.exercise.value + index}>                           
+                            <td>{ex.exercise.value}</td>
+                            <td>{ex.distance.value}</td> 
+                            <td>{ex.rest.value}</td> 
+                            <td>{ex.numberApproaches.value}</td>
+                        </tr>
+                    ))} 
+                </tbody>
+            </table>
+        );
+    };
+
     return (
         <div className="row">
           <div className="col-12">
@@ -22,26 +48,22 @@ function TrainingsList() {
             <table className="table">
                 <thead>
                     <tr>
-                        <th scope="col">Date</th>
-                        <th scope="col">Title</th>
-                        <th scope="col">Time</th>
-                        <th scope="col">Exercise</th>
-                        <th scope="col">Distance</th>
-                        <th scope="col">Rest</th>
-                        <th scope="col">Number Approaches</th>
+                        <th scope="col" className='col-2'>Date</th>
+                        <th scope="col" className='col-2'>Title</th>
+                        <th scope="col" className='col-2'>Time</th>
+                        <th scope="col" className='col-6'>Exercises</th>                      
                     </tr>
                 </thead>
                 <tbody>
                     {trainingsArray.map(({ date, trainings }) => (
                         trainings.map((training, index) => (
-                            <tr key={`${date}-${index}`}>
+                            <tr key={`${training.name}-${index}`}>
                                 <td>{format(new Date(date), 'dd/MM/yyyy')}</td>
-                                <td>{training.title.value}</td>
-                                <td>{training.exercise.value}</td>
-                                <td>{training.time.value}</td>
-                                <td>{training.distance.value}</td>
-                                <td>{training.rest.value}</td>
-                                <td>{training.numberApproaches.value}</td>
+                                <td>{training.name}</td>
+                                <td>{training.time}</td>
+                                <td>
+                                    {exercisesTable(training.exercises)}
+                                </td>
                             </tr>
                         ))
                     ))}
